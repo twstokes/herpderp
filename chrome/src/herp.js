22918d7cae39ffcdc5b0d1eb9410d4c0ef4300bf
Tanner Stokes <tanner@tannr.com>
2016-10-18 20:21:23 -0400

    No jQuery dependency in Chrome

--- chrome/src/herp.js
+++ chrome/src/herp.js
@@ -1,38 +1,41 @@
-function randomDerp() {
+// selectors for comments
+var selectors = ['.Ct', '.comment-renderer-text-content'];
+
+// builds a string with random herps and derps
+var derpString = function() {
+  var randomLength = Math.floor(Math.random()*20)+1;
+  var returnString = '';
+
+  for (x=0; x<=randomLength; x++) {
+    returnString += (Math.floor(Math.random()*2) ? 'herp ' : 'derp ');
+  }
   
-    this.derpOriginal = $(this).html();
-    
-    $(this).click(function() {
-      $(this).html(this.derpOriginal);
-    });
-    
-    var randomLength = (Math.floor(Math.random()*20)+1);
-    var wordArray = new Array();
-
-    for(var x = 0; x < randomLength; x++) {
-
-      randomBit = (Math.floor(Math.random()*2));
-
-      if(randomBit == 1) {
-        wordArray[x] = 'herp';
-      } else {
-        wordArray[x] = 'derp';
-      }
-    }
-
-    // add derped class
-    $(this).addClass("derped");
-    
-    return wordArray.join(' ');
-
+  return returnString;
 }
 
-// only select un-derped elements
-$('.Ct, .comment-renderer-text-content').not('.derped').html(randomDerp);
+// derps a comment
+var derpComment = function(comment) {
+    // preserve the original contents
+    comment.derpOriginal = comment.innerHTML;
 
+    // revert to the original when clicked
+    comment.onclick = function() {
+      comment.innerHTML = comment.derpOriginal;
+    };
+    
+    // add derped class
+    comment.classList.add('derped');
 
+    // change the contents
+    comment.innerHTML = derpString();
+}
+
+// build the full selector string
+var derpSelectorString = selectors.map(function(sel) {
+  return sel + ':not(.derped)';
+}).join(", ");
+
+// every 100 milliseconds, derp any un-derped elements
 setInterval(function() {
-  // only select un-derped elements
-  $('.Ct, .comment-renderer-text-content').not('.derped').html(randomDerp);
+  document.querySelectorAll(derpSelectorString).forEach(derpComment);
 }, 100);
-
