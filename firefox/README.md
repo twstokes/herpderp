6ec4bbd4c939302f1c1bdab5464a444a218c94d9
Tanner Stokes <tanner@tannr.com>
2017-11-12 01:21:27 -0500

    Remove old Firefox directory

--- firefox/README.md
+++
@@ -1,6 +0,0 @@
-# Building for Firefox
-
-1. `npm install -g jpm`
-2. `jpm test`
-3. `jpm run` or `jpm run -b /Applications/FirefoxDeveloperEdition.app`
-4. `jpm xpi`
--- firefox/src/index.js
+++
@@ -1,8 +0,0 @@
-var data = require("sdk/self").data;
-var pageMod = require("sdk/page-mod");
-
-pageMod.PageMod({
-    include: ["https://apis.google.com/*", "https://plus.googleapis.com/*", "https://www.youtube.com/*"],
-    contentScriptWhen: 'ready',
-    contentScriptFile: [data.url("herp.js")]
-});
