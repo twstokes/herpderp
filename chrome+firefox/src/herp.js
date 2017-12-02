dc98562b1a27cbbec0102077a6a5382dcafe4df9
atomheartother <atomheartother@gmail.com>
2017-12-02 18:07:30 +0000

    clicking now switches between derped and underped

--- chrome+firefox/src/herp.js
+++ chrome+firefox/src/herp.js
@@ -9,21 +9,29 @@ var derpString = function() {
   for (x=0; x<=randomLength; x++) {
     returnString += (Math.floor(Math.random()*2) ? 'herp ' : 'derp ');
   }
-  
+
   return returnString;
 }
 
 // derps a comment
 var derpComment = function(comment) {
+
+    // indicate that this comment is derped
+    comment.derped = true;
+
     // preserve the original contents
     comment.derpOriginal = comment.textContent;
 
-    // revert to the original when clicked
+    // switch between derped and underped when clicked
     comment.onclick = function() {
-      comment.textContent = comment.derpOriginal;
+      // invert derp value
+      comment.derped = !comment.derped;
+
+      // change contents to either a new random derp string or the original comment
+      comment.textContent = comment.derped ? derpString() : comment.derpOriginal;
     };
-    
-    // add derped class
+
+    // add derped class for the selector
     comment.classList.add('derped');
 
     // change the contents
