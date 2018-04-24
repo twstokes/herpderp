c37d8e6a0ace992d8334a7dc997d46f801add00c
Tanner Stokes <tanner@tannr.com>
2018-04-23 22:14:16 -0400

    ES6

--- chrome+firefox/src/herp.js
+++ chrome+firefox/src/herp.js
@@ -1,41 +1,36 @@
 // selectors for comments
-var selectors = ['#content-text'];
+const selectors = ["#content-text"];
+// build the full selector string
+const derpSelectorString = selectors
+  .map(sel => sel + ":not(.derped)")
+  .join(", ");
 
 // builds a string with random herps and derps
-var derpString = function() {
-  var randomLength = Math.floor(Math.random()*20)+1;
-  var returnString = '';
+const derpString = () => {
+  const maxLength = 20;
+  const randomLength = Math.floor(Math.random() * maxLength) + 1;
 
-  for (x=0; x<=randomLength; x++) {
-    returnString += (Math.floor(Math.random()*2) ? 'herp ' : 'derp ');
+  let string = "";
+  for (let x = 0; x <= randomLength; x++) {
+    string += Math.floor(Math.random() * 2) ? "herp " : "derp ";
   }
-  
-  return returnString;
-}
+
+  return string;
+};
 
 // derps a comment
-var derpComment = function(comment) {
-    // preserve the original contents
-    comment.derpOriginal = comment.textContent;
-
-    // revert to the original when clicked
-    comment.onclick = function() {
-      comment.textContent = comment.derpOriginal;
-    };
-    
-    // add derped class
-    comment.classList.add('derped');
-
-    // change the contents
-    comment.textContent = derpString();
-}
-
-// build the full selector string
-var derpSelectorString = selectors.map(function(sel) {
-  return sel + ':not(.derped)';
-}).join(", ");
+const derpComment = comment => {
+  // preserve the original contents
+  comment.derpOriginal = comment.textContent;
+  // revert to the original when clicked
+  comment.onclick = () => (comment.textContent = comment.derpOriginal);
+  // add derped class
+  comment.classList.add("derped");
+  // change the contents
+  comment.textContent = derpString();
+};
 
 // every 100 milliseconds, derp any un-derped elements
-setInterval(function() {
+setInterval(() => {
   document.querySelectorAll(derpSelectorString).forEach(derpComment);
 }, 100);
